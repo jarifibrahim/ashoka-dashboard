7584743a22b2f229ec6e8e0bcc3ea37351f373e1
Ibrahim Jarif <jarifibrahim@gmail.com>
2016-12-05 00:32:31 +0530

    Dashboard and Team overview partially completed
    - All files were moved and hence they all appear modified
    - Add field updation via dialog box to Dashboard and Team page
    - Multiple models, admin site entried and forms updated

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     url(r'^fellow/(?P<hash_value>[\w]+)/$',
         views.fellow_submit, name='fellow_survey'),
     url(r'^dashboards/(?P<dashboard_id>[0-9]+)/$',
-        views.details, name='detail'),
+        views.dashboard_overview, name='dashboard_overview'),
     url(r'^thanks/$', views.thanks, name='thanks'),
     url(r'^urls/$', views.show_urls, name='show_urls'),
+    url(r'^update/$', views.update_value, name='update'),
+    url(r'^teams/(?P<team_id>[0-9]+)/$', views.team_detail, name='team_display'),
     url(r'^$', views.home, name='index'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Dashboard, Team
-from .forms import ConsultantSurveyForm, FellowSurveyForm
 from django.contrib import messages
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from .models import Dashboard, Team, Member
+from .forms import ConsultantSurveyForm, FellowSurveyForm
 
 
 @login_required
@@ -18,16 +18,36 @@ def home(request):
 
 
 @login_required
-def details(request, dashboard_id):
+def dashboard_overview(request, dashboard_id):
     """ Dashboard Display page"""
     dashboard = get_object_or_404(Dashboard, id=dashboard_id)
     if not dashboard.teams.all():
         messages.error(
             request, 'No Teams found in this dashboard. Please create teams')
-    allTeams = dashboard.teams.all()
+
+    all_teams = dashboard.teams.all()
+    team_list, lrp_list, working_document = (list() for _ in range(3))
+    consultant_requests, fellow_requests = (list() for _ in range(2))
+    lrp_comment_and_teamid, status_and_teamid = (list() for _ in range(2))
+
+    for team in all_teams:
+        team_list.append({'teamid': team.id, 'name': team.name})
+        lrp_list.append(team.get_members_with_role("LRP"))
+        working_document.append(team.working_document)
+        consultant_requests.append(team.consultant_request)
+        fellow_requests.append(team.fellow_request)
+        status_and_teamid.append({'teamid': team.id, 'status': team.status})
+        lrp_comment_and_teamid.append(
+            {'teamid': team.id, 'comment': team.lrp_comment})
+
     context = {
-        'teams': [str(t.id) + ": " + str(t.name) for t in allTeams],
-        'LRPs': [t.get_members_with_role("LRP") for t in allTeams]
+        'teams': team_list,
+        'LRPs': lrp_list,
+        'working_document': working_document,
+        'consultant_request': consultant_requests,
+        'fellow_requests': fellow_requests,
+        'status': status_and_teamid,
+        'lrp_comment': lrp_comment_and_teamid,
     }
     return render(request, "dashboard_display.html", context=context)
 
@@ -54,7 +74,7 @@ def decode_data(data):
 
 
 def consultant_submit(request, hash_value):
-    """ Survey from request and response """
+    """ Consultant Survey from request and response """
     team_id = decode_data(hash_value)
     team_object = get_object_or_404(Team, pk=team_id)
     if request.method == 'POST':
@@ -65,7 +85,10 @@ def consultant_submit(request, hash_value):
             entry = form.save(commit=False)
             # Form does not contain team id. So add it.
             entry.team = team_object
+            # Save new instance
             entry.save()
+            # Save many to many data from the Form
+            form.save_m2m()
             messages.success(
                 request, 'Your Response has been saved Successfully. \
                           Thank you!')
@@ -83,6 +106,7 @@ def thanks(request):
 
 
 def fellow_submit(request, hash_value):
+    """ Fellow Survey from request and response """
     dashboard_id = decode_data(hash_value)
     get_object_or_404(Dashboard, pk=dashboard_id)
     if request.method == "POST":
@@ -100,20 +124,158 @@ def fellow_submit(request, hash_value):
 
 @login_required
 def show_urls(request):
+    """ Show all form urls """
     dashboard_list = list(
         Dashboard.objects.values_list('id', 'name'))
     fellow_survey_urls = [{
-        'url': encode_data(id),
-        'name': name
-    }for id, name in dashboard_list]
+                              'url': encode_data(d_id),
+                              'name': name
+                          } for d_id, name in dashboard_list]
 
     team_list = list(
         Team.objects.values_list('id', 'name'))
     consultant_survey_urls = [{
-        'url': encode_data(id),
-        'name': name
-    }for id, name in team_list]
+                                  'url': encode_data(t_id),
+                                  'name': name
+                              } for t_id, name in team_list]
 
     return render(request, "show_urls.html",
                   context={'f_urls': fellow_survey_urls,
                            'c_urls': consultant_survey_urls})
+
+
+def update_team_value(request, change_name, change_field_id):
+    """
+    Updates the team value. This function is called only by update_value when
+    a value related to the team is to be updated. The value is sent by a dialog
+    form.
+    :param request:         Request object that contains all the required values
+    :param change_name:     Type of change to be done.
+                            Possible values are "status" and "comment"
+    :param change_field_id: Id of the form field that contains the new value
+    :return:                True is successful else False
+    """
+    # Extract team id from the teamId string
+    team_id = request.POST.get('teamId')
+    try:
+        team_object = Team.objects.get(pk=team_id)
+    except Team.DoesNotExist:
+        messages.error(request, "Failed to update value. Invalid team id")
+        return False
+
+    # Change Team status color
+    if change_name.lower() == "status":
+        try:
+            team_object.status = request.POST[change_field_id]
+            team_object.save()
+            messages.success(request, "Team status updated successfully.")
+        except Exception as e:
+            messages.debug(request, "Failed to update value. " + str(e))
+            return False
+
+    # Change Team comment
+    elif change_name.lower() == "comment":
+        try:
+            team_object.lrp_comment = request.POST[change_field_id]
+            team_object.save()
+            messages.success(request, "Team LRP comment updated successfully")
+        except Exception as e:
+            messages.debug(request, "Failed to update value. " + str(e))
+            return False
+    else:
+        messages.debug(request, "Unknown action " + change_name)
+        return False
+    return True
+
+
+def update_member_value(request, change_name, change_field_id):
+    """
+    Updates the member object value. This function is called only by
+    update_value when a value related to a member is to be updated. The value
+    is sent by a dialog box which contains the form.
+    :param request:         Request object that contains all the required values
+    :param change_name:     Type of change to be done.
+                            Possible value is "comment"
+    :param change_field_id: Id of the form field that contains the new value
+    :return:                True is successful else False
+    """
+
+    # Extract member id from the memebrId string
+    member_id = request.POST.get('memberId')
+
+    try:
+        member_object = Member.objects.get(pk=member_id)
+    except Team.DoesNotExist:
+        messages.error(request, "Failed to update value. Invalid team id")
+        return False
+
+    # Change Team comment
+    if change_name.lower() == "comment":
+        try:
+            member_object.comment = request.POST[change_field_id]
+            member_object.save()
+            messages.success(request, "Member comment updated successfully")
+        except Exception as e:
+            messages.debug(request, "Failed to update value. " + str(e))
+            return False
+        return True
+    return False
+
+
+@login_required
+def update_value(request):
+    """
+    Update dashboard values. Request is sent via dialogue boxes on the
+    dashboard page.
+    """
+
+    if request.method != "POST":
+        HttpResponseRedirect(reverse("index"))
+
+    # Contains possible fields that can be changed
+    # Format:
+    #    change name: form field id
+    possible_field_change = {
+        'status': 'newStatusColor',
+        'LRP_comment': 'LRPComment',
+        'Member_comment': 'memberComment',
+    }
+    response_url = request.META.get('HTTP_REFERER', 'index')
+    messages.debug(request, request.POST)
+
+    # Change team status color
+    if possible_field_change['status'] in request.POST:
+        status = update_team_value(
+            request, 'status', possible_field_change['status'])
+        if not status:
+            messages.error(request, "Failed to update status value.")
+
+    # Change LRP Comment
+    elif possible_field_change['LRP_comment'] in request.POST:
+        status = update_team_value(
+            request, 'comment', possible_field_change['LRP_comment'])
+        if not status:
+            messages.error(request, "Failed to update LRP Comment value.")
+
+    # Change Member comment
+    elif possible_field_change['Member_comment'] in request.POST:
+        status = update_member_value(
+            request, 'comment', possible_field_change['Member_comment'])
+        if not status:
+            messages.error(request, "Failed to update Member Comment value.")
+
+    else:
+        messages.error(request, "Error: Unknown action.")
+    return HttpResponseRedirect(response_url)
+
+
+def team_detail(request, team_id):
+    team_object = get_object_or_404(Team, pk=team_id)
+
+    context = {
+        'teamid': team_object.id,
+        'team_name': team_object.name,
+        'team_members': team_object.members.all(),
+        'consultant_responses': team_object.consultant_surveys.all(),
+    }
+    return render(request, "team_display.html", context=context)
